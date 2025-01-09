a23d4ee6824fdc9f07ca7b8b06c1885fdafb4fc2
Sarthak Chauhan <sarthakc981@gmail.com>
2025-01-09 17:38:55 +0530

    Add files via upload

---
+++ LinkedList/linkedList.java
@@ -0,0 +1,59 @@
+package LinkedList;
+
+public class linkedList {
+    Node head;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    // add first
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // add last
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currentNode = head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+    }
+
+    public void printList() {
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.print("Null");
+    }
+
+    public static void main(String[] args) {
+        linkedList list = new linkedList();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.addFirst("This");
+        list.addLast("Linked List");
+        list.printList();
+    }
+}
