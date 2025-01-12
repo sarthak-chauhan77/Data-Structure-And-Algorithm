52d68e460dd218eca7250fb5036d074ae2b0b58f
Sarthak Chauhan <sarthakc981@gmail.com>
2025-01-12 21:28:59 +0530

    Add files via upload

---
+++ String/string.java
@@ -0,0 +1,15 @@
+package String;
+
+public class string {
+    public static void main(String[] args) {
+        String str1 = "You'r";
+        String str2 = " Awesome";
+        System.out.println(str1.concat(str2));
+        System.out.println(str1.charAt(2));
+        System.out.println(str2.charAt(2));
+        System.out.println(str2.substring(4));
+        System.out.println(str2.contains("s"));
+        System.out.println(str2.equals(str1));
+
+    }
+}
